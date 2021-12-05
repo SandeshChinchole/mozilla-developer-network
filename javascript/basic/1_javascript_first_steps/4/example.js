bb9a21fcbd7ca0eb071587baef99ee47b49812a5
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-12-05 15:47:08 -0500

    completed variables guide

---
+++ javascript/basic/1_javascript_first_steps/4/example.js
@@ -0,0 +1,9 @@
+const bird = {
+    species: 'Kestrel'
+}
+
+console.log(bird.species); // Kestrel
+
+bird.species = 'Striated Caracara';
+
+console.log(bird.species); // Striated Caracara
---
+++ javascript/basic/1_javascript_first_steps/5/example.js
@@ -0,0 +1,3 @@
+let myNumber = '74'; // string
+myNumber = Number(myNumber) + 4; // using Number() constructor to convert string to number
+console.log(myNumber);
