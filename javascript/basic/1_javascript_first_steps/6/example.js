3eba3516b09134bb06dbf9124614e7447a8e76f2
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-12-09 16:44:01 -0500

    updated example.js

--- javascript/basic/1_javascript_first_steps/6/example.js
+++ javascript/basic/1_javascript_first_steps/6/example.js
@@ -1,7 +1,3 @@
-function oddEven(num1, num2) {
-    return num1 === num2 ? 'both are equal' 
-    : num1 > num2 ? 'num1 is greater than num2'
-    : 'num2 is greater than num1';
-}
+const doubler = num => num * 2;
 
-console.log(5, 3)
+console.log(doubler(2));
