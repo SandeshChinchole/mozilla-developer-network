b9541f5a593400c43598088b8fae96b6eac7bab0
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-12-11 16:17:29 -0500

    example.js

--- javascript/basic/1_javascript_first_steps/6/example.js
+++ javascript/basic/1_javascript_first_steps/6/example.js
@@ -1,3 +1,11 @@
 const myPromise = new Promise((resolve, reject) => {
-    
+    if (condotion) {
+        resolve('promise is resolved')
+    } else {
+        reject('promise is rejected')
+    }
+
+    myPromise.then(result => {
+        console.log(result)
+    })
 });
