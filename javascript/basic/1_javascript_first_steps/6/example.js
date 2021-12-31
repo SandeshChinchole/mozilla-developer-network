40da6c90f3ad0503e5b471091256ec561d2ba659
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-12-31 11:56:16 -0500

    reafctored example.js

--- javascript/basic/1_javascript_first_steps/6/example.js
+++ javascript/basic/1_javascript_first_steps/6/example.js
@@ -2,4 +2,4 @@ function endsWith(str, target) {
     return str.slice(str.length - target.length) === target;
 }
 
-console.log(endsWith('chello', 'o'));
+console.log(endsWith('happy new year', 'o'));
