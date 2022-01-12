a9193c08554a5cd696634f414b68bd1166d8f26b
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2022-01-12 09:34:44 -0500

    refactored

--- javascript/basic/1_javascript_first_steps/6/example.js
+++ javascript/basic/1_javascript_first_steps/6/example.js
@@ -2,4 +2,4 @@ function endsWith(str, target) {
     return str.slice(str.length - target.length) === target;
 }
 
-console.log(endsWith('statue of liberty', 'k'));
+console.log(endsWith('empire state', 'k'));
