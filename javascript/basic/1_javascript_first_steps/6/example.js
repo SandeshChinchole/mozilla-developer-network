4982919efdbc04cf39209726746e577537e06ce8
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2022-01-13 13:04:06 -0500

    refactored

--- javascript/basic/1_javascript_first_steps/6/example.js
+++ javascript/basic/1_javascript_first_steps/6/example.js
@@ -2,4 +2,4 @@ function endsWith(str, target) {
     return str.slice(str.length - target.length) === target;
 }
 
-console.log(endsWith('empire state', 'k'));
+console.log(endsWith('united states', 'k'));
