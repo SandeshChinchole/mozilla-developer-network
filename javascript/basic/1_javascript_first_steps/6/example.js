d45017bb7e7ab628159a4060b92ceab9b6262e6c
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2022-01-14 16:22:29 -0500

    refactored

--- javascript/basic/1_javascript_first_steps/6/example.js
+++ javascript/basic/1_javascript_first_steps/6/example.js
@@ -2,4 +2,4 @@ function endsWith(str, target) {
     return str.slice(str.length - target.length) === target;
 }
 
-console.log(endsWith('united states', 'k'));
+console.log(endsWith('east coast', 'k'));
