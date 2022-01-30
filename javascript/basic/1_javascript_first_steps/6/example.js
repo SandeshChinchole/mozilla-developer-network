b4d799d1b725bded65e98b3ec48ce1bc0ebf001b
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2022-01-30 09:18:08 -0500

    refactored

--- javascript/basic/1_javascript_first_steps/6/example.js
+++ javascript/basic/1_javascript_first_steps/6/example.js
@@ -1,29 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-function showHide() {
-  const [showChild, setShowChild] = React.useState('');
-  const element = (
-    <>
-      <label>
-        <input
-          type="checkbox"
-          checked={showChild}
-          onChange={e => setShowChild(e.target.checked)}
-        />{' '}
-        show child
-      </label>
-      <div
-        style={{
-          padding: 10,
-          margin: 10,
-          height: 30,
-          width: 30,
-          border: 'solid',
-        }}
-      >
-        {showChild ? showChild : null}
-      </div>
-      <button type="submit">Submit</button>
-    </>
-  )
-  
+const books = [
+  {
+    id: 1,
+    author: 'Amelia Hepworth',
+    title: 'I Love You to the Moon and Back',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL906_SR906,600_.jpg',
+  },
+
+  {
+    id: 2,
+    author: 'Valerie Bertinelli',
+    title: 'Enough Already: Learning to Love the Way I Am Today',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81oUqf2aiiS._AC_UL906_SR906,600_.jpg',
+  },
+
+  {
+    id: 3,
+    author: 'Amelia Hepworth',
+    title: 'I Love You to the Moon and Back',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL906_SR906,600_.jpg',
+  },
+];
+
+function BookList() {
+  return (
+    <section className='booklist'>
+      {books.map((book, index) => {
+        return <Book key={book.id} {...book} />;
+      })}
+    </section>
+  );
 }
+
+const Book = ({ author, title, img }) => {
+  //   const { author, title, img } = props;
+
+  return (
+    <article className='book'>
+      <img src={img} alt='' />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
