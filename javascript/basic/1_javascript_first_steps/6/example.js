6cb59dd8a5dd606847c26e968d15388d76171272
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2022-01-31 09:00:00 -0500

    refactored code

--- javascript/basic/1_javascript_first_steps/6/example.js
+++ javascript/basic/1_javascript_first_steps/6/example.js
@@ -1,50 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { useState } from 'react';
 
-const books = [
-  {
-    id: 1,
-    author: 'Amelia Hepworth',
-    title: 'I Love You to the Moon and Back',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL906_SR906,600_.jpg',
-  },
+const UseStateCounter = () => {
+  const [value, setValue] = useState(0);
 
-  {
-    id: 2,
-    author: 'Valerie Bertinelli',
-    title: 'Enough Already: Learning to Love the Way I Am Today',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81oUqf2aiiS._AC_UL906_SR906,600_.jpg',
-  },
-
-  {
-    id: 3,
-    author: 'Amelia Hepworth',
-    title: 'I Love You to the Moon and Back',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL906_SR906,600_.jpg',
-  },
-];
-
-function BookList() {
-  return (
-    <section className='booklist'>
-      {books.map((book, index) => {
-        return <Book key={book.id} {...book} />;
-      })}
-    </section>
-  );
-}
-
-const Book = ({ author, title, img }) => {
-  //   const { author, title, img } = props;
+  const handleReset = () => setValue(0);
 
   return (
-    <article className='book'>
-      <img src={img} alt='' />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-    </article>
+    <>
+      <section style={{ margin: '4rem 0' }}>
+        <h1>regular counter</h1>
+        <h2>{value}</h2>
+        <button className='btn' onClick={() => setValue(value - 1)}>
+          decrease
+        </button>
+        <button className='btn' onClick={handleReset}>
+          reset
+        </button>
+        <button className='btn' onClick={() => setValue(value + 1)}>
+          increase
+        </button>
+      </section>
+    </>
   );
 };
 
-ReactDOM.render(<BookList />, document.getElementById('root'));
+export default UseStateCounter;
