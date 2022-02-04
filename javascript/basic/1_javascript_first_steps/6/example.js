02699a0733fd8b4efea94a2b1f30a500e68d6a11
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2022-02-04 08:33:23 -0500

    refactored

--- javascript/basic/1_javascript_first_steps/6/example.js
+++ javascript/basic/1_javascript_first_steps/6/example.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import Setup from './tutorial/1-useState/setup/1-error-example';
+
+const books = [
+  {
+    id: 1,
+    author: 'Amelia Hepworth',
+    title: 'I Love You to the Moon and Back',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL906,600_.jpg',
+  },
+  {
+    id: 2,
+    author: 'Valerie Bertinelli',
+    title: 'Enough Already: Learning to Love the Way I Am Today',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81oUqf2aiiS._AC_UL906_S,600_.jpg',
+  },
+];
+
+const Book = ({ author, title, img }) => {
+  return (
+    <article>
+      <img style={{ width: '200px' }} src={img} alt={title} />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
 
 function App() {
   return (
-    <div className='container'>
-      <Setup />
-    </div>
+    <section className='App'>
+      {books.map((book, index) => {
+        return <Book key={index} {...book} />;
+      })}
+    </section>
   );
 }
 
