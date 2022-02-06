cd84bddb044504d0bb39a9cd2caa66d54bb65e2b
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2022-02-06 08:12:03 -0500

    refactored

--- javascript/basic/1_javascript_first_steps/6/example.js
+++ javascript/basic/1_javascript_first_steps/6/example.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
+import React, { useState, useEffect } from 'react';
+const url = 'https://api.github.com/users/QuincyLarson';
+const MultipleReturns = () => {
+  return <h2>multiple returns</h2>;
+};
 
-function App() {
-  return (
-    <div className='container'>
-      <Setup />
-    </div>
-  );
-}
-
-export default App;
+export default MultipleReturns;
