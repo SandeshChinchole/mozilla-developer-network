e0606ce9da0759b5733d33c76dc170ea7a153d83
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2022-02-17 11:08:50 -0500

    refactoted

--- javascript/basic/1_javascript_first_steps/6/example.js
+++ javascript/basic/1_javascript_first_steps/6/example.js
@@ -1,69 +1,26 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch';
+import React, { useEffect, useState } from 'react';
 
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/javascript-store-products';
+// 1. filter employees from /data/employees.json to only display bankers
+// 2. sort bankers by satisfationRating
 
-// every time props or state changes, component re-renders
-
-const Index = () => {
-  const { products } = useFetch(url);
-  const [count, setCount] = useState(0);
-  const [cart, setCart] = useState(0);
-
-  const addToCart = useCallback(() => {
-    setCart(cart + 1);
-  }, [cart]);
+const BankEmployee = (props) => {
+  const bankerFilter = props.user.type === 'banker';
 
   return (
-    <>
-      <h1>Count : {count}</h1>
-      <button className='btn' onClick={() => setCount(count + 1)}>
-        click me
-      </button>
-      <h1 style={{ marginTop: '3rem' }}>Cart: {cart}</h1>
-      <BigList products={products} addToCart={addToCart} />
-    </>
+    <div>
+      {bankerFilter && (
+        <div className='bank-employee'>
+          <h1>{props.children}</h1>
+          <span>{props.user.userName}</span>
+          <br />
+          <span>Name: {props.user.name}</span>
+          <br />
+          <span>
+            Customer Satisfaction Rating: {props.user.satisfactionRating}
+          </span>
+        </div>
+      )}
+    </div>
   );
 };
-
-const BigList = React.memo(({ products, addToCart }) => {
-  useEffect(() => {
-    console.log('big list called');
-  });
-
-  return (
-    <section className='products'>
-      {products.map((product) => {
-        return (
-          <SingleProduct
-            key={product.id}
-            {...product}
-            addToCart={addToCart}
-          ></SingleProduct>
-        );
-      })}
-    </section>
-  );
-});
-
-const SingleProduct = ({ fields, addToCart }) => {
-  useEffect(() => {
-    console.count('single product called');
-  });
-
-  let { name, price } = fields;
-  price = price / 100;
-  const image = fields.image[0].url;
-
-  return (
-    <article className='product'>
-      <img src={image} alt={name} />
-      <h4>{name}</h4>
-      <p>${price}</p>
-      <button onClick={addToCart}>Add to Cart</button>
-    </article>
-  );
-};
-export default Index;
+export default React.memo(BankEmployee);
